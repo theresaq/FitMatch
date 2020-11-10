b40853a0a38f18b6311f5c96cc4e222d0486ef41
Theresa Qiu <yqiu22@my.bcit.ca>
2020-11-10 12:12:56 -0800

    updated login.html and login.js to connect app to firestore database, changed firebase configuration info

--- scripts/firebase_api_fitmatch.js
+++ scripts/firebase_api_fitmatch.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAckox_zIqhEH-bpkKPAXbo1W7Xfc8cKOo",
-    authDomain: "fitmatch-50907.firebaseapp.com",
-    databaseURL: "https://fitmatch-50907.firebaseio.com",
-    projectId: "fitmatch-50907",
-    storageBucket: "fitmatch-50907.appspot.com",
-    messagingSenderId: "964225984537",
-    appId: "1:964225984537:web:638cbde4774c79e8682288"
+    apiKey: "AIzaSyAzoHv8rvnN_yh9-xjfa8vVln39PadObo0",
+    authDomain: "fit-match.firebaseapp.com",
+    databaseURL: "https://fit-match.firebaseio.com",
+    projectId: "fit-match",
+    storageBucket: "fit-match.appspot.com",
+    messagingSenderId: "388515857656",
+    appId: "1:388515857656:web:cf47d7d33ba25743f2270d"
 };
 
 // Initialize Firebase
--- scripts/login.js
+++ scripts/login.js
@@ -21,7 +21,7 @@ var uiConfig = {
                         email: user.email
                     }).then(function () {
                         console.log("New user added to firestore");
-                        window.location.assign("main.html");
+                        window.location.assign("home.html");
                     })
                     .catch(function (error) {
                         console.log("Error adding new user: " + error);
@@ -39,7 +39,7 @@ var uiConfig = {
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
-    signInSuccessUrl: 'main.html',
+    signInSuccessUrl: 'home.html',
     signInOptions: [
         // Leave the lines as is for the providers you want to offer your users.
         //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -50,9 +50,9 @@ var uiConfig = {
         //firebase.auth.PhoneAuthProvider.PROVIDER_ID
     ],
     // Terms of service url.
-    tosUrl: 'main.html',
+    tosUrl: 'home.html',
     // Privacy policy url.
-    privacyPolicyUrl: 'main.html',
+    privacyPolicyUrl: 'home.html',
     accountChooserEnabled: false
 };
 // The start method will wait until the DOM is loaded.
