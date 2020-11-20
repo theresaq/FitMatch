c408e7433f42574600cf1b9ff0e61c575cce470a
Theresa Qiu <yqiu22@my.bcit.ca>
2020-11-20 09:41:57 -0800

    added colour for landing, login, and profile page with my_app_palette3.css

--- scripts/login.js
+++ scripts/login.js
@@ -3,17 +3,7 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
     callbacks: {
         signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-            // User successfully signed in.
-            // Return type determines whether we continue the redirect automatically
-            // or whether we leave that to developer to handle.
-            //------------------------------------------------------------------------------------------
-            // The code below is modified from default snippet provided by the FB documentation.
-            //
-            // If the user is a "brand new" user, then create a new "user" in your own database.
-            // Assign this user with the name and email provided.
-            // Before this works, you must enable "Firestore" from the firebase console.
-            // The Firestore rules must allow the user to write. 
-            //------------------------------------------------------------------------------------------
+
             var user = authResult.user;
             if (authResult.additionalUserInfo.isNewUser) {
                 db.collection("users").doc(user.uid).set({
@@ -41,13 +31,8 @@ var uiConfig = {
     signInFlow: 'popup',
     signInSuccessUrl: 'profile.html',
     signInOptions: [
-        // Leave the lines as is for the providers you want to offer your users.
-        //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        //firebase.auth.GithubAuthProvider.PROVIDER_ID,
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
-        //firebase.auth.PhoneAuthProvider.PROVIDER_ID
+
     ],
     // Terms of service url.
     tosUrl: 'home.html',
@@ -55,6 +40,5 @@ var uiConfig = {
     privacyPolicyUrl: 'home.html',
     accountChooserEnabled: false
 };
-// The start method will wait until the DOM is loaded.
-// Inject the login interface into the HTML
+
 ui.start('#firebaseui-auth-container', uiConfig);
--- scripts/readUserName.js
+++ scripts/readUserName.js
@@ -3,8 +3,6 @@ firebase.auth().onAuthStateChanged(function(user) {
         function readUserName(){
             db.collection("users").doc(user.uid)
             .onSnapshot(function(snap){
-                console.log(snap.data());   //print the document fields of "01"
-                console.log(snap.data().name);   //spelled EXACTLY as the firestore
                 document.getElementById("UserName").innerText = snap.data().name;
             })
         }
