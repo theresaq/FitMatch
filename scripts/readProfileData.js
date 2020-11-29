98284b3ad19925ed5f734ae506045b188334087c
Theresa Qiu <yqiu22@my.bcit.ca>
2020-11-28 18:37:23 -0800

    updated reading/writing profile data functionality

--- scripts/readProfileData.js
+++ scripts/readProfileData.js
@@ -1,19 +1,18 @@
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        function readUserName(){
+        function readProfileData(){
             db.collection("users").doc(user.uid)
             .onSnapshot(function(snap){
                 document.getElementById("Age").innerText = snap.data().DOB;
                 document.getElementById("City").innerText = snap.data().city;
+                document.getElementById("Province").innerText = snap.data().province;
                 document.getElementById("Gender").innerText = snap.data().gender;
                 document.getElementById("activity1").innerText = snap.data().activity1;
                 document.getElementById("skill1").innerText = snap.data().skill1;
-                
             })
         }
-        
-        readUserName();
+        readProfileData();
     } else {
-      // do nothing
+      console.log("Error reading user data.");
     }
   });
--- scripts/saveProfile.js
+++ scripts/saveProfile.js
@@ -3,15 +3,13 @@
   firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
           save.addEventListener("click", function() {
-            db.collection("users").doc(user.uid).set({
-                name: user.displayName,
-                email: user.email,
+            db.collection("users").doc(user.uid).update({
                 gender: document.getElementById("Gender").value,
                 DOB: document.getElementById("Birthday").value,
                 city: document.getElementById("City").value,
                 province: document.getElementById("Province").value,
                 activity1: document.getElementById("Activity-1").value,
-                skill1: document.getElementById("SkillLevel-1").value
+                skill1: document.getElementById("SkillLevel-1").value,
             }).then(function() {
                 console.log("Data successfully saved.");
             })
