afa953c9820ace087abae1acfedd7560505018eb
Kevin Baumann <baumannkev@gmail.com>
2020-12-01 20:52:09 -0800

    updated favorites functionality for the aside

--- scripts/favButton.js
+++ scripts/favButton.js
@@ -3,6 +3,7 @@ $(".heart.fa").click(function() {
 
     if ($("#heart").hasClass('fa-heart')) {
         console.log("ON");
+        
         // Save to database
         firebase.auth().onAuthStateChanged(function (user) {
             db.collection("users").doc(user.uid).update({
@@ -18,5 +19,82 @@ $(".heart.fa").click(function() {
             })
         })
     }
+    
 });
 
+
+// FIRST FAVORITED USER 
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        function readUserName(){
+            db.collection("users").doc(user.uid)
+            .onSnapshot(function(snap){
+                var firstFav = snap.data().hearts[0];
+                document.getElementById("firstFavName").innerText = snap.data().hearts[0];
+                
+                // NOW LOOK FOR THE PHOTO AND THE MAIN ACTIVITY
+                
+                function getUsersWithQuery() {
+                    db.collection("users")
+                        .where("name", "==", firstFav)
+                        .get()
+                        .then(function (snap) {
+                            snap.forEach(function (doc) {
+                                console.log("is from BC: ", doc.data().name);
+                                document.getElementById("firstActivity").innerText = doc.data().activity1;
+                                var picUrl = doc.data().photo;
+                                $("#firstPic").before("<img src='" + picUrl + "' class='list-img-top'>")
+                                
+                            })
+                
+                        })
+                }
+                
+                getUsersWithQuery();
+
+            })
+        }
+
+        readUserName();
+    } else {
+      // do nothing
+    }
+  });
+
+  // SECOND FAVORITED USER 
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+        function readUserName(){
+            db.collection("users").doc(user.uid)
+            .onSnapshot(function(snap){
+                var secondFav = snap.data().hearts[1];
+                document.getElementById("secondFavName").innerText = snap.data().hearts[1];
+                
+                // NOW LOOK FOR THE PHOTO AND THE MAIN ACTIVITY
+                
+                function getUsersWithQuery() {
+                    db.collection("users")
+                        .where("name", "==", secondFav)
+                        .get()
+                        .then(function (snap) {
+                            snap.forEach(function (doc) {
+                                console.log("is from BC: ", doc.data().name);
+                                document.getElementById("secondActivity").innerText = doc.data().activity1;
+                                var picUrl = doc.data().photo;
+                                $("#secondPic").before("<img src='" + picUrl + "' class='list-img-top'>")
+                                
+                            })
+                
+                        })
+                }
+                
+                getUsersWithQuery();
+
+            })
+        }
+
+        readUserName();
+    } else {
+      // do nothing
+    }
+  });
